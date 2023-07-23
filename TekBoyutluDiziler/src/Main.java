dd5f0619f363fc042b53a55398a6f4effb68975e
Gizot <tastangizem@gmail.com>
2023-07-23 15:07:08 +0300

    second process done

--- TekBoyutluDiziler/src/Main.java
+++ TekBoyutluDiziler/src/Main.java
@@ -4,13 +4,20 @@ public class Main {
        // double[] list2 = new double[10];
        //double[] list1;
        //list1 = new double[20];
-        String[] days = {"Pazartesi","Salı", "Çarşamba", "Perşembe", "Cuma"};
+       /* String[] days = {"Pazartesi","Salı", "Çarşamba", "Perşembe", "Cuma"};
         String[] weekendDays = new String[]{"Cumartesi","Pazar"};
         System.out.println(weekendDays[0]);
         System.out.println(days.length); //array'in uzunluğunu (boyutunu) verir.
         days[0] = "Perşembe"; //0. index in değerini değiştirdik
         for(int i=0; i<= (days.length-1); i++) {
             System.out.println(days[i]);
-        }
+        } */
+
+        int cars[], count=3;
+        cars = new int[count];
+        for(int i=0; i<cars.length; i++)
+            cars[i] = (i+1)*2;
+        for(int j=0; j<cars.length; j++)
+            System.out.print(cars[j] + ",");
     }
 }
